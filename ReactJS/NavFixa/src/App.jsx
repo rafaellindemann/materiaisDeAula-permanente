d830cb0c6195cfc178f0b5a64d1ac703fc3c6120
Rafael Lindemann Duarte <rafaelld@gmail.com>
2024-09-25 21:36:17 -0300

    add demo de layout com navbar fixa superior

---
+++ ReactJS/NavFixa/src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav>Aqui é a navbar fixa de 60px</nav>
+      <main>
+        <p>Esta página é um demo de um layout com navbar superior fixa. O mais balaio possível.</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+        <p>Aqui é o conteúdo principal, com scroll</p>
+      </main>
+    </>
+  )
+}
+
+export default App
